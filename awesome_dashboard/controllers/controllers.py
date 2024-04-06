e3051bd6999643d987621018c82a5c7725c8acac
Yassir Irfan <yassirirfan7@gmail.com>
2024-04-06 14:05:23 +0530

    [ADD] Dashboard Item Template

--- awesome_dashboard/controllers/controllers.py
+++ awesome_dashboard/controllers/controllers.py
@@ -8,29 +8,16 @@ from odoo.http import request
 
 logger = logging.getLogger(__name__)
 
+
 class AwesomeDashboard(http.Controller):
     @http.route('/awesome_dashboard/statistics', type='json', auth='user')
     def get_statistics(self):
-        """
-        Returns a dict of statistics about the orders:
-            'average_quantity': the average number of t-shirts by order
-            'average_time': the average time (in hours) elapsed between the
-                moment an order is created, and the moment is it sent
-            'nb_cancelled_orders': the number of cancelled orders, this month
-            'nb_new_orders': the number of new orders, this month
-            'total_amount': the total amount of orders, this month
-        """
-
-        return {
-            'average_quantity': random.randint(4, 12),
-            'average_time': random.randint(4, 123),
-            'nb_cancelled_orders': random.randint(0, 50),
-            'nb_new_orders': random.randint(10, 200),
-            'orders_by_size': {
-                'm': random.randint(0, 150),
-                's': random.randint(0, 150),
-                'xl': random.randint(0, 150),
-            },
-            'total_amount': random.randint(100, 1000)
-        }
+        def fetch_count(model):
+            return request.env[model].search_count([])
+        return [
+            {'id': 2, 'size': 2, 'title': 'Customers',
+             'content': fetch_count('res.partner')},
+            {'id': 3, 'size': 1, 'title': 'Leads',
+             'content': fetch_count('crm.lead')}
+        ]
 
--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,18 +1,32 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { ControlPanel } from "@web/search/control_panel/control_panel";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { userService } from "@web/core/user_service";
 
+class DashboardItem extends Component {
+    static template = "awesome_dashboard.DashboardItem";
+    static props = {
+        size: 1
+    }
+    redirectView(ev){
+        console.log('view_name', ev.currentTarget)
+    }
+}
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
-    static components = { Layout };
+    static components = { Layout, DashboardItem };
     setup() {
         this.action = useService("action");
+        this.rpc = useService("rpc");
         this.notificationService = useService("notification");
+        onWillStart(async () => {
+            const result = await this.rpc("/awesome_dashboard/statistics", {a: 1, b: 2});
+            this.data = result
+        });
     }
     showNotification() {
         this.notificationService.add("Hello", {
@@ -21,8 +35,7 @@ class AwesomeDashboard extends Component {
         })
     }
     openSettings() {
-        this.action.doAction("base_setup.action_general_configuration");
+        this.action.doAction("base.action_partner_form");
     }
 }
-
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);

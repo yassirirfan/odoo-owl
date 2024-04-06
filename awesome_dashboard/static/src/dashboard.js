ba3158a3832b66eebf326f00f8261d10e8443510
Yassir Irfan <yassirirfan7@gmail.com>
2024-04-06 16:45:56 +0530

    [UPD] func: showNotification

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -6,6 +6,7 @@ import { ControlPanel } from "@web/search/control_panel/control_panel";
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
 import { userService } from "@web/core/user_service";
+import {  _t } from "@web/core/l10n/translation";
 
 class DashboardItem extends Component {
     static template = "awesome_dashboard.DashboardItem";
@@ -29,10 +30,21 @@ class AwesomeDashboard extends Component {
         });
     }
     showNotification() {
-        this.notificationService.add("Hello", {
-            title: "test",
-            type: "success"
-        })
+        this.notificationService.add(_t("Critical error occurred. Please contact support."), {
+            title: "Error",
+            type: "danger",
+            buttons: [{
+                        onClick: () => {
+                            this.actionService.doAction(
+                                "base_setup.action_general_configuration"
+                            );
+                            closeFn();
+                        },
+                        primary: true,
+                        name: "Go to Settings",
+                    },
+            ],
+        });
     }
     openSettings() {
         this.action.doAction("base.action_partner_form");

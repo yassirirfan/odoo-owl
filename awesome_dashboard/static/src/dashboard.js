bb5e4a788530af32e03071450fd46bf762c6235e
Yassir Irfan <yassirirfan7@gmail.com>
2024-04-06 12:25:58 +0530

    [rm] post_notifications & [UPD] awesome_dashboard

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -4,10 +4,25 @@ import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { ControlPanel } from "@web/search/control_panel/control_panel";
 import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { userService } from "@web/core/user_service";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
     static components = { Layout };
+    setup() {
+        this.action = useService("action");
+        this.notificationService = useService("notification");
+    }
+    showNotification() {
+        this.notificationService.add("Hello", {
+            title: "test",
+            type: "success"
+        })
+    }
+    openSettings() {
+        this.action.doAction("base_setup.action_general_configuration");
+    }
 }
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);

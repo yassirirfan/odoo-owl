244eed97366bb415850542ac040521400476196b
Yassir Irfan <yassirirfan7@gmail.com>
2024-04-20 16:04:02 +0530

    [UPD] auto_reload

--- kanban_auto_reload/static/src/js/kanban_controller.js
+++ kanban_auto_reload/static/src/js/kanban_controller.js
@@ -1,9 +1,8 @@
 /** @odoo-module */
-
 import { patch } from "@web/core/utils/patch";
 import { KanbanController } from "@web/views/kanban/kanban_controller";
 import { useEffect, useState, onMounted, onWillUnmount, onWillStart  } from "@odoo/owl";
-var intervalId;
+
 patch(KanbanController.prototype, {
     setup() {
         super.setup(...arguments);
@@ -11,17 +10,22 @@ patch(KanbanController.prototype, {
         this.state = useState({current_count: false});
         onMounted(async () => {
             this.intervalId = setInterval(async () => {
+                // Fetches the recent count of records in the model.
                 var newCount = await self.model.root.fetchCount()
                 if (this.state.current_count){
+                    // Check if the count fetched during previous interval and current is equal.
                     if (newCount != this.state.current_count){
+                        // Reloads the state if there is variation between counts of different intervals.
                         self.model.action.loadState();
                     }
                 } else {
+                    // Initial assignment of count
                     this.state.current_count = newCount
                 }
-            }, 5000);
+            }, 15000);
         });
         onWillUnmount(() => {
+            // Clears the interval if the component is destroyed.
             clearInterval(this.intervalId);
         });
     },

da5b6658e075fb3e74d6e6c10f929bf0c1a2e002
Yassir Irfan <yassirirfan7@gmail.com>
2024-05-18 10:29:09 +0530

    [ADD] pos_disable_numpad

---
+++ pos_disable_numpad/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "POS Disable Numpad",
+    'summary': """Starting module for "Discover Point Of Sale" """,
+    'description': """Starting module for Point Of Sale""",
+    'author': "Yassir Irfan",
+    'website': "https://www.odoo.com/",
+    'category': 'Tutorials',
+    'version': '17.0.1.0.0.1',
+    'application': True,
+    'installable': True,
+    'depends': ['base', 'web'],
+    'data': [
+    ],
+    'assets': {
+    },
+    'license': 'AGPL-3'
+}

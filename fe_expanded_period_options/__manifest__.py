3fc1d24dd5253ec84a9b2364ee70132a57e5d95b
Yassir Irfan <59363528+yassirirfan@users.noreply.github.com>
2025-03-19 17:38:19 +0530

    Update __manifest__.py

--- fe_expanded_period_options/__manifest__.py
+++ fe_expanded_period_options/__manifest__.py
@@ -1,33 +1,10 @@
 # -*- coding: utf-8 -*-
-#############################################################################
-#
-#    e-Sustavi d.o.o.
-#
-#    Copyright (C) 2025-TODAY e-Sustavi d.o.o.(<http://www.e-sustavi.hr>).
-#    Author: e-Sustavi d.o.o.(<http://www.e-sustavi.hr>)
-#
-#    You can modify it under the terms of the GNU AFFERO
-#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
-#
-#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
-#    (AGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
 {
     "name": "Extended Invoice/Bill Period Filters",
     "version": "16.0.1.0.0",
     "category": "Accounting",
     "summary": "Expands available period filters for invoices and bills in Odoo Accounting.",
-    'author': 'e-Sustavi d.o.o.',
-    'company': 'e-Sustavi d.o.o.',
-    'maintainer': 'e-Sustavi d.o.o.',
-    'website': "http://www.e-sustavi.hr",
+    'author': 'Yassir Irfan',
     "license": "LGPL-3",
     "depends": ["account", "web", "spreadsheet"],
     "data": [
